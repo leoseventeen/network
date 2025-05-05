1f36bea94464ff77e16be8371981c2c0152e65a5
leoseventeen <90679875+leoseventeen@users.noreply.github.com>
2025-05-05 15:53:29 +0930

    Update sr.h

--- sr.h
+++ sr.h
@@ -1,15 +1,16 @@
+/* sr.h */
 #ifndef SR_H
 #define SR_H
 
 #include "emulator.h"
 
-/* Sender-side functions */
+/* Sender-side API */
 void A_init(void);
 void A_output(struct msg message);
 void A_input(struct pkt packet);
 void A_timerinterrupt(void);
 
-/* Receiver-side functions */
+/* Receiver-side API */
 void B_init(void);
 void B_input(struct pkt packet);
 void B_output(struct msg message);
